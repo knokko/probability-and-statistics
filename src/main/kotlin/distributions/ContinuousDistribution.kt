a448c90506ab37f75a0c9ce1d8cf9c2baa1251b9
knokko <knokogator@hotmail.com>
2021-05-05 11:53:58 +0200

    Make it possible to override things in ContinuousDistribution

--- src/main/kotlin/distributions/ContinuousDistribution.kt
+++ src/main/kotlin/distributions/ContinuousDistribution.kt
@@ -5,9 +5,9 @@ import java.lang.Double.max
 import java.lang.Double.min
 import kotlin.math.sqrt
 
-class ContinuousDistribution(val f: (Double) -> Double, val lowerBound: Double? = null, val upperBound: Double? = null) {
+open class ContinuousDistribution(val f: (Double) -> Double, val lowerBound: Double? = null, val upperBound: Double? = null) {
 
-    fun chanceBetween(lowerBound: Double? = null, upperBound: Double? = null): Double {
+    open fun chanceBetween(lowerBound: Double? = null, upperBound: Double? = null): Double {
         val combinedLowerBound: Double? = if (this.lowerBound == null) {
             lowerBound;
         } else if (lowerBound == null) {
@@ -27,11 +27,11 @@ class ContinuousDistribution(val f: (Double) -> Double, val lowerBound: Double? 
         return computeIntegral(f, combinedLowerBound, combinedUpperBound)
     }
 
-    val mean: Double by lazy {
+    open val mean: Double by lazy {
         computeIntegral({ x -> x * f(x)}, lowerBound, upperBound)
     }
 
-    val variance: Double by lazy {
+    open val variance: Double by lazy {
         computeIntegral({ x -> (x - mean) * (x - mean) * f(x)}, lowerBound, upperBound)
     }
 
