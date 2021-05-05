f701a395b2c2ce0e7fc3067ce4978a7e22013fd5
knokko <knokogator@hotmail.com>
2021-05-05 12:14:49 +0200

    Make the tests of ContinuousNormalDistribution.chanceBetween stricter

--- src/test/kotlin/distributions/TestContinuousNormalDistribution.kt
+++ src/test/kotlin/distributions/TestContinuousNormalDistribution.kt
@@ -8,16 +8,16 @@ class TestContinuousNormalDistribution {
     @Test
     fun testChanceBetween() {
         val standard = ContinuousNormalDistribution(0.0, 1.0)
-        assertEquals(0.9066, standard.chanceBetween(upperBound = 1.32), 0.001)
-        assertEquals(0.9987, standard.chanceBetween(upperBound = 3.0), 0.001)
-        assertEquals(0.0735, standard.chanceBetween(lowerBound = 1.45), 0.001)
-        assertEquals(0.9512, standard.chanceBetween(-2.34, 1.76), 0.001)
-        assertEquals(1.0, standard.chanceBetween(), 0.001)
+        assertEquals(0.9066, standard.chanceBetween(upperBound = 1.32), 0.0001)
+        assertEquals(0.9987, standard.chanceBetween(upperBound = 3.0), 0.0001)
+        assertEquals(0.0735, standard.chanceBetween(lowerBound = 1.45), 0.0001)
+        assertEquals(0.9512, standard.chanceBetween(-2.34, 1.76), 0.0001)
+        assertEquals(1.0, standard.chanceBetween(), 0.0001)
 
         val custom1 = ContinuousNormalDistribution(10.0, 2.0)
-        assertEquals(0.9332, custom1.chanceBetween(upperBound = 13.0), 0.001)
-        assertEquals(0.6915, custom1.chanceBetween(lowerBound = 9.0), 0.001)
-        assertEquals(0.1587, custom1.chanceBetween(-2.0, 8.0), 0.001)
+        assertEquals(0.9332, custom1.chanceBetween(upperBound = 13.0), 0.0001)
+        assertEquals(0.6915, custom1.chanceBetween(lowerBound = 9.0), 0.0001)
+        assertEquals(0.1587, custom1.chanceBetween(-2.0, 8.0), 0.0001)
         assertEquals(1.0, custom1.chanceBetween(), 0.001)
     }
 
